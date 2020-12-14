7f7c4c1151f211b59304871083aa1524d1821cb2
veronica scislewski <45470627+cybervee@users.noreply.github.com>
2020-12-14 15:41:42 -0300

    page 32

---
+++ Capítulo 1 • Chapter 1/pagina 32.py
@@ -0,0 +1,6 @@
+romance = ('Vidas Secas', '1938')
+ano = romance[1]
+print(ano)
+
+romances_Graciliano = [('Caetês', '1933'), ('São Bernardo', '1934'), ('Angústia', '1936'), ('Vidas Secas', '1938')]
+print(romances_Graciliano[1])
